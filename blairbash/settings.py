ecab187384de6a47d0da4975341b029cf11dc2f3
Kevin Higgs <github@kevinhiggs.com>
2018-08-14 18:05:29 -0400

    django admin colors

--- blairbash/settings.py
+++ blairbash/settings.py
@@ -33,14 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'dal',
     'dal_select2',
+    'qdb',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taggit',
-    'qdb',
+    'taggit'
 ]
 
 MIDDLEWARE = [
