d9daaa9d2d96e7a8aea98fcaad311294da575abb
Kevin Higgs <github@kevinhiggs.com>
2018-08-16 15:54:09 -0400

    clean up

--- blairbash/settings/development.py
+++ blairbash/settings/development.py
@@ -3,10 +3,3 @@ from .base import *
 SECRET_KEY = 'development_key'
 DEBUG = True
 ALLOWED_HOSTS = []
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
